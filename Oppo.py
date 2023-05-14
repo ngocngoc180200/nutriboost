e589d88cdc8358152a191feb363bec78167492f8
ngoccngoc <ngoccthao1802@gmail.com>
2023-05-15 00:41:47 +0700

    Update equal to equal

--- Oppo.py
+++ Oppo.py
@@ -1,5 +1,5 @@
 a = "nutriboost"
 b = "cocacola"
 
-print("Nutriboost is equal Cocacola!") if a >= b else print("Nutriboost is not equal Cocacola")
+print("Nutriboost is equal Cocacola!") if a == b else print("Nutriboost is not equal Cocacola")
 print("Nutriboost is equal Cocacola!") if a <= b else print("Nutriboost is not equal Cocacola")
