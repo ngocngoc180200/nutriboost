5de49a77a3389d5f03a7a42dda9dbd05775be27a
ngocngoc180200 <132736807+ngocngoc180200@users.noreply.github.com>
2023-05-15 00:21:19 +0700

    Update Oppo.py from equal to greater

--- Oppo.py
+++ Oppo.py
@@ -1,4 +1,4 @@
 a = "nutriboost"
 b = "cocacola"
 
-print("Nutriboost is equal Cocacola!") if a == b else print("Nutriboost is not equal Cocacola")
+print("Nutriboost is equal Cocacola!") if a < b else print("Nutriboost is not equal Cocacola")
